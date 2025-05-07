7c122cee860ee4c79e384e3836f89fd9e293681d
K-Dog <103394438+kayle-henry@users.noreply.github.com>
2025-05-06 19:52:29 -0500

    Create blogDB.sql

---
+++ blogDB.sql
@@ -0,0 +1,53 @@
+//4 tables; Users, Topics, Articles, and Comments as required.
+   //grabbed from gfulenwi/BlogDatabase.
+   //connection line : $mysqli = new mysqli("127.0.01","bloguser","blogpass","blogbd");
+
+DROP DATABASE IF EXISTS blogdb;
+CREATE DATABASE blogdb;
+
+use blogdb;
+drop user if exists 'bloguser'@'localhost';
+
+CREATE USER IF NOT EXISTS 'bloguser'@'localhost' identified by 'blogpass';
+grant all on blogdb.* to 'bloguser'@'localhost';
+
+create table users(
+   userID int AUTO_INCREMENT,
+   username varchar(30) not null, index(username),
+   lastname varchar(50),
+   firstname varchar(30),
+   password varchar(30),
+   email varchar(255),
+   role varchar(30),
+   lastModified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   primary key(userID)
+)engine=innodb;
+
+create table topics(
+   topID int AUTO_INCREMENT,
+   name varchar(50),
+   description varchar(255),
+   lastModified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   primary key(topID)
+)engine=innodb;
+
+create table articles(
+   artID int AUTO_INCREMENT,
+   topID int; //look at this
+   authorID int NOT NULL, index(authorID),
+   catID int NOT NULL, index(catID),
+   title varchar(255),
+   image varchar(255),
+   content text,
+   lastModified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   primary key(artID)
+)engine=innodb;
+
+create table comments(
+   comID int AUTO_INCREMENT,
+   authorID int NOT NULL, index(authorID),
+   artID int NOT NULL, index(artID),
+   content varchar(255),
+   lastModified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+   primary key(comID)
+)engine=innodb;
