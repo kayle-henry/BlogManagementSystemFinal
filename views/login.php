a69db8380e797ae54a34b1827e0f70168b104ce3
K-Dog <103394438+kayle-henry@users.noreply.github.com>
2025-05-08 18:59:22 -0500

    Update login.php

--- views/login.php
+++ views/login.php
@@ -10,8 +10,16 @@
                             <input type="text" class="form-control mb-3" id="username" name="username" placeholder="Enter your Username" required>
                             <label for="passwd" class="form-label">Password</label>
                             <input type="text" class="form-control mb-3" id="passwd" name="passwd" placeholder="Enter your Password" required>
+                        <div class="d-flex gap-2 mt-3">
                             <button type="submit" class="btn btn-primary">Login</button>
                         </form>
+                        <form action="controller.php" method="GET">
+                            <input type="hidden" name="page" value="home">
+                            <button type="submit" class="btn btn-secondary">Cancel</button>"
+                            <input type="hidden" name="page" value="newUser">
+                            <button type="submit" class="btn btn-secondary">Create Account</button>"
+                        </form>
+                        </div>
                     </div>
                 </div>      
             </div>
