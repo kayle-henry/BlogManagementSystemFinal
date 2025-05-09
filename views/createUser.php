6f2d9a717e417d9a1e813c00171b7bfe8b092532
K-Dog <103394438+kayle-henry@users.noreply.github.com>
2025-05-08 19:10:24 -0500

    Update createUser.php

--- views/createUser.php
+++ views/createUser.php
@@ -1,4 +1,19 @@
-    <div class="container">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CS 2033 | Add Product</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<style>
+    .container {
+        padding-top: 20px;
+    }
+</style>
+<body>
+    
+<div class="container">
         <div class="row">
             <div class="col">
                 <div class="card">
@@ -31,4 +46,8 @@
                 </div>      
             </div>
         </div>
-    </div>
+</div>
+    
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
